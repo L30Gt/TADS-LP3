92ecc2fe727dc313ee4cb2412edf7cdca72b4ea9
L30Gt <leogt.dev@gmail.com>
2026-08-17 22:21:54 -0300

    aula 17-08

--- src/main/java/br/edu/ifsp/orderflow/domain/ItemPedido.java
+++ src/main/java/br/edu/ifsp/orderflow/domain/ItemPedido.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.orderflow.domain;
 
+import java.math.BigDecimal;
+
 public class ItemPedido {
     private final Produto produto;
     private final int quantidade;
@@ -20,5 +22,9 @@ public class ItemPedido {
     @Override
     public String toString() {
         return this.quantidade + "x " + this.produto.getNome();
+    }
+
+    public BigDecimal calcularSubtotal() {
+        return this.produto.getPreco().multiply(BigDecimal.valueOf(this.quantidade));
     }
 }
